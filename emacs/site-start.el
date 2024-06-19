c0f9cef3fb8de38166aa8af904c52b41b107e830
wangzh <wangzh@localhost>
2024-06-19 12:41:40 +0800

    emacs: simplify

--- emacs/site-start.el
+++ emacs/site-start.el
@@ -1,4 +1,12 @@
-(setq user-init-file "E:/files/config/emacs/init.el")
-(setq work-init-file "E:/files/config/emacs/work.el")
+;; This file should be put at: C:\Program Files\Emacs\emacs-29.3_2\share\emacs\site-lisp\site-start.el
+
+;; https://emacs.stackexchange.com/a/48029
+;; Shortcut settings: "C:\Program Files\Emacs\emacs-29.3_2\bin\emacsclientw.exe" -n -c -a "" --server-file "D:\chin-home\.emacs.d\server\server"
+
+(setq user-init-file       "D:/files/config/emacs/init.el"
+      user-emacs-directory "D:/chin-home/.emacs.d"
+      default-directory    "F:/playground-data")
+
+(setenv "HOME" "D:/chin-home")
+
 (load user-init-file)
-(load work-init-file)
