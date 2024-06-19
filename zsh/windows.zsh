ef0f7bf43cb08782b7cc67fbf92ee14c3cbcb1f3
wangzh <wangzh@locolhost>
2024-06-19 09:02:17 +0800

    zsh: windows, add some alias

--- zsh/windows.zsh
+++ zsh/windows.zsh
@@ -2,9 +2,11 @@
 
 export CHIN_WORK_DAYS_DIR="/e/work"
 export CHIN_FILES_DIR="/e/files"
+export CHIN_PG_DIR="/e/playground-firm"
 
 export PATH=$CHIN_FILES_DIR/config/scripts:$PATH
 export PATH=/ucrt64/bin/:$PATH
+export PATH=/D/Tools/cmd/poppler-24.02.0/Library/bin:$PATH
 
 alias td='mkdir -p "$CHIN_WORK_DAYS_DIR/$(date +%y%m/%d)" && cd "$CHIN_WORK_DAYS_DIR/$(date +%y%m/%d)"'
 
@@ -17,3 +19,13 @@ cptd() {
 
     cp -r "$1" "$CHIN_WORK_DAYS_DIR/$(date +%y%m/%d)"
 }
+
+topg() {
+    local fn="$(basename "$1")"
+
+    cp -r "$1" "$CHIN_PG_DIR/$(date -u +%y%m-%d)-$1"
+}
+
+alias rg="rg --path-separator //"
+
+alias jdk8="export JAVA_HOME=/C/Users/w/.jdks/corretto-1.8.0_362/; export PATH=/C/Users/w/.jdks/corretto-1.8.0_362/bin:$PATH"
