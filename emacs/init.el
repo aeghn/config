1888ed7bc0f0504d231dafbca7b93acd15335308
aeghn <aeghn@outlook.com>
2024-06-21 18:16:25 +0800

    emacs: magit settings
    1. start magit full screen
    2. magit scripts

--- emacs/init.el
+++ emacs/init.el
@@ -726,3 +726,18 @@
           (interactive)
           (save-some-buffers)
           (kill-emacs)))
+(block! "Magit"
+	:packages ("magit")
+
+	(setq magit-display-buffer-function
+              (lambda (buffer)
+		(display-buffer
+		 buffer (if (and (derived-mode-p 'magit-mode)
+				 (memq (with-current-buffer buffer major-mode)
+                                       '(magit-process-mode
+					 magit-revision-mode
+					 magit-diff-mode
+					 magit-stash-mode
+					 magit-status-mode)))
+			    nil
+			  '(display-buffer-same-window))))))
