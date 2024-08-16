a20aa67051f92e6de415ac529e830ce51c9b0acc
wangzh <wangzh@localhost>
2024-08-16 11:49:13 +0800

    rime: update
    1. custom trigger key for date, time, timestamp
    2. init readme

--- zsh/vendor.zsh
+++ zsh/vendor.zsh
@@ -84,5 +84,5 @@ cptd() {
 
 if $_CHIN_IS_MSYS2; then
     alias rg="rg --path-separator //"
-    alias jdk8='export JAVA_HOME=/D/Tools/cmd/data/corretto-1.8.0/; export PATH=/D/Tools/cmd/data/corretto-1.8.0/:$PATH'
+    alias jdk8='export JAVA_HOME=/D/Tools/cmd/data/corretto-1.8.0/; export PATH=/D/Tools/cmd/data/corretto-1.8.0/bin:$PATH'
 fi
