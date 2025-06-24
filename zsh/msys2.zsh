20ef53b047e00a1835f1632530320b32356d35d7
wangzh <wangzh@localhost>
2025-06-24 13:27:28 +0800

    windows: update

--- zsh/msys2.zsh
+++ zsh/msys2.zsh
@@ -1,8 +1,7 @@
-
 alias rg="rg --path-separator //"
-alias jdk8='export JAVA_HOME=/D/Tools/cmd/data/corretto-1.8.0/; export PATH=/D/Tools/cmd/data/corretto-1.8.0/bin:$PATH'
-alias jdk17='export JAVA_HOME=/d/Tools/jdks/corretto-17; export PATH=/d/Tools/jdks/corretto-17/bin:$PATH'
+alias jdk8='export JAVA_HOME=/d/Tools/jdks/jdk-8/; export PATH=$JAVA_HOME/bin:$PATH'
+alias jdk17='export JAVA_HOME=/d/Tools/jdks/jdk-17; export PATH=$JAVA_HOME/bin:$PATH'
+alias jdk21='export JAVA_HOME=/d/Tools/jdks/jdk-21; export PATH=$JAVA_HOME/bin:$PATH'
 
 export PATH=/d/Tools/cmd:$PATH
 export PATH=/ucrt64/bin/:$PATH
-export PATH=/D/Tools/cmd/poppler-24.02.0/Library/bin:$PATH
