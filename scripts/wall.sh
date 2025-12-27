32ee49ac608d80cea377789d4f757fb050803e4d
aeghn <aeghn@outlook.com>
2025-12-27 19:18:42 +0800

    scripts: update

--- scripts/wall.sh
+++ scripts/wall.sh
@@ -2,4 +2,4 @@
 
 pkill swaybg
 # swaybg -m fill -i "$(find ~/files/media/pics/wallpapers -type f | shuf -n1)" &! 
-swaybg -m fill -i /home/chin/files/media/pics/wallpapers/fedora-light-blue.png &!
+swaybg -m fill -i /home/chin/files/media/pics/wallpapers/wynand-van-poortvliet-4AmyOdXZAQc-unsplash.jpg &!
